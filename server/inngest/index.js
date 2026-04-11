a3076e1dd1b7ccc50c34fd59d8580079fa43c152
UmerAli <malikumerali14@gmail.com>
2026-04-11 15:14:24 +0500

    2nd commit

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,12 +1,14 @@
 import { Inngest } from "inngest";
-import User from "../models/User";
+import User from '../models/User.js'
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "pingup-app" });
 
 const syncUserCreation = inngest.createFunction(
-    { id: "sync-user-from-clerk" },
-    { event: "clerk/user.created" },
+    {
+        id: "sync-user-from-clerk",
+        triggers: { event: "clerk/user.created" },
+    },
     async ({ event }) => {
         const { id, email_addresses, first_name, last_name, image_url } = event.data;
         let username = email_addresses[0].split('@')[0];
@@ -27,13 +29,15 @@ const syncUserCreation = inngest.createFunction(
 
         await User.create(userData);
 
-    },
+    }
 );
 
 //Function to update user data in database 
 const syncUserUpdation = inngest.createFunction(
-    { id: "update-user-from-clerk" },
-    { event: "clerk/user.updated" },
+    {
+        id: "update-user-from-clerk",
+        triggers: { event: "clerk/user.updated" },
+    },
     async ({ event }) => {
         const { id, email_addresses, first_name, last_name, image_url } = event.data;
         let username = email_addresses[0].split('@')[0];
@@ -46,20 +50,22 @@ const syncUserUpdation = inngest.createFunction(
 
         await User.findByIdAndUpdate(id, updateUserData);
 
-    },
+    }
 );
 
 
 //Function to delete user data from database 
 const syncUserDeletion = inngest.createFunction(
-    { id: "delete-user-from-clerk" },
-    { event: "clerk/user.deleted" },
+    {
+        id: "delete-user-from-clerk",
+        triggers: { event: "clerk/user.deleted" },
+    },
     async ({ event }) => {
         const { id } = event.data;
 
         await User.findByIdAndDelete(id);
 
-    },
+    }
 );
 
 // Create an empty array where we'll export future Inngest functions
